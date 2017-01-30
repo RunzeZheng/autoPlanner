efec362738f7f031e55615261cfe81c394298c31
zheng394 <zheng394@umn.edu>
2017-01-29 18:14:35 -0600

    Runze Zheng

---
+++ app/src/main/java/com/autoplanner/sorter.java
@@ -0,0 +1,23 @@
+package com.autoplanner;
+
+import android.util.ArraySet;
+
+/**
+ * Created by zrzzg on 12/29/2016.
+ */
+public class sorter {
+    public  ArraySet<Task> deadlineSort(ArraySet<Task> a){
+        ArraySet<Task> result = a;
+        return result;
+    }
+
+    public  ArraySet<Task> durationSort(ArraySet<Task> a){
+        ArraySet<Task> result = a;
+        return result;
+    }
+
+    public  ArraySet<Task> optimizedSort(ArraySet<Task> a){
+        ArraySet<Task> result = a;
+        return result;
+    }
+}
